f5bdc6ac88df23a1d7fa9c090127cce8f383abcd
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-08-14 22:08:39 +0000

    comit

--- script/idtoolbox.lua
+++
@@ -1,5 +0,0 @@
-local InsertService = game:GetService("InsertService")
-local assetId = 119435101634997 -- ID de um modelo público da Toolbox
-
-local asset = InsertService:LoadAsset(assetId)
-asset.Parent = game.Players.LocalPlayer.Backpack
