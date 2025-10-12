d1d6a024f0d3e95d941ec38c5c65e3721eb023f0
Adrian Razini Rangel <160901383+AdrainRazini@users.noreply.github.com>
2025-10-12 20:17:49 +0000

    Comit upppps

--- script/Legends_Of_Speeds.lua
+++ script/Legends_Of_Speeds.lua
@@ -164,20 +164,16 @@ function FarmFastOrb()
 			-- dispara FireServer sem limite de batch, só respeitando o maxQueue
 			while queueCount < maxQueue and AF.FarmFastOrb do
 				queueCount += 1
-
+				local args = {
+					[1] = "collectOrb",
+					[2] = Val_Orb,
+					[3] = "City"
+				}
 				task.spawn(function()
-					local args = {
-						[1] = "collectOrb",
-						[2] = Val_Orb,
-						[3] = "City"
-					}
 					game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args))
-
-					-- decrementa o contador após envio
 					queueCount -= 1
 				end)
 			end
-
 			-- intervalo mínimo, só para não travar completamente o jogo
 			task.wait(AF_Timer.FarmFastOrb_Timer) -- espera o tempo configurado
 		end
